cf7de7c263729f93d7c2471120a2ce37df2b7514
Mitja Maren <mico.maren@gmail.com>
2016-11-20 21:10:01 +0000

    Priprava strani za vsako temo ter testiranje

---
+++ scripts/themes.js
@@ -0,0 +1,7 @@
+function theme1(){
+	document.getElementById("txtpath").innerHTML="<a href='index.html'>home</a>/<a href='theme.html'>C</a>/";
+	
+}
+
+//var link1=document.getElementById("theme1");
+//link1.onclick=theme1();
