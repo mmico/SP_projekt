90e234749545e15a2910f527795147bf2990c253
Mitja Maren <mico.maren@gmail.com>
2016-12-11 19:33:08 +0000

    Dopolnjevanje

--- scripts/themes.js
+++ scripts/themes.js
@@ -1,7 +1,36 @@
-/*function theme1(){
-	document.getElementById("txtpath").innerHTML="<a href='index.html'>home</a>/<a href='theme.html'>C</a>/";
-	
-}*/
+function load(){
+	var t=location.search.split('t=')[1];
+	//console.log(t);
+	switch (t){
+		case '1': 
+			document.getElementById("txtpath").innerHTML="<a href='index.html'>home</a>/<a href='theme.html?t=1'>C</a>/"
+			break;
+		case '2': 
+			document.getElementById("txtpath").innerHTML="<a href='index.html'>home</a>/<a href='theme.html?t=2'>C++</a>/"
+			break;
+		case '3': 
+			document.getElementById("txtpath").innerHTML="<a href='index.html'>home</a>/<a href='theme.html?t=3'>C#</a>/"
+			break;
+		case '4': 
+			document.getElementById("txtpath").innerHTML="<a href='index.html'>home</a>/<a href='theme.html?t=4'>Java</a>/"
+			break;
+		case '5': 
+			document.getElementById("txtpath").innerHTML="<a href='index.html'>home</a>/<a href='theme.html?t=5'>Javascript</a>/"
+			break;
+		case '6': 
+			document.getElementById("txtpath").innerHTML="<a href='index.html'>home</a>/<a href='theme.html?t=6'>HTML</a>/"
+			break;
+		case '7': 
+			document.getElementById("txtpath").innerHTML="<a href='index.html'>home</a>/<a href='theme.html?t=7'>CSS</a>/"
+			break;
+		case '8': 
+			document.getElementById("txtpath").innerHTML="<a href='index.html'>home</a>/<a href='theme.html?t=8'>Python</a>/"
+			break;
+		case '9': 
+			document.getElementById("txtpath").innerHTML="<a href='index.html'>home</a>/<a href='theme.html?t=9'>Prolog</a>/"
+			break;
+		default: break;
+	}
+}
 
-//var link1=document.getElementById("theme1");
-//link1.onclick=theme1();
+window.onload=load;
